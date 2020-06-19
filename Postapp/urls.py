bb3baa49d4141f5446d69df061b4361c71afb0a8
fadi <fadimvp@gmail.com>
2020-06-19 16:11:42 +0300

    add  m all

---
+++ Postapp/urls.py
@@ -0,0 +1,33 @@
+from django.urls import path 
+
+ 
+from . import views
+
+urlpatterns=[
+    path('',views.home,name='home'),
+    path('postmabna',views.post_mabna,name='postmabna'),
+    path('asmer',views.post_asmer,name='asmer'),
+    path('hotels',views.post_hotels,name='hotels'),
+
+
+    path('about',views.about,name='about'),
+    path('invmabna',views.invmabna,name='invmabna'),
+    path('indexar',views.index_ar,name='indexar'),
+    path('contactus',views.contactus,name='contactus'),
+    path('contactusar',views.contactusar,name='contactusar'),
+    path('all_project',views.all_project,name='all_project'),
+
+    path('postmabnaar',views.post_mabnaar,name='postmabnaar'),
+    path('asmerar',views.post_asmerar,name='asmerar'),
+    path('hotelsar',views.post_hotelsar,name='hotelsar'),
+    path('owl',views.owl,name='owl'),
+
+
+
+
+
+
+
+
+
+]
